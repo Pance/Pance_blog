57a176bd58c24ed7c1c253783f2c3385cca67b33
Pance <pbarnett888@gmail.com>
2014-02-01 13:55:48 -0600

    Add some content to front page

--- template/index.html.clj
+++ template/index.html.clj
@@ -10,9 +10,17 @@
 ; Template is compiled with hiccup
 (header
   (:title site)
-	"The blog of Peter Barnett."
+    [:p.text-center "Hello, I'm Peter Barnett"]
+    [:p.text-center "I am a developer[github] who believes code and design can be beautiful."]
+    [:p.text-center "I work in an accelerated development program as a software developer at HEB Grocery Company[link]."]
+    [:p.text-center
+      (link "github.com/Pance" "https://github.com/Pance")
+      "  "
+      (link "soundcloud.com/DJPance" "https://soundcloud.com/djpance")]
+    [:p.text-center "I am a Texan, hacker, gamer and a bedroom DJ. I am interested in the web, personal data tracking, automation, people, cars, fitness and electronic dance music."]
+    [:p.text-center "I'm currently building a todo-list web app using Clojure called todo-repl[link]."]
+
 	[:br]
-  (link "github.com/Pance" "https://github.com/Pance")
   )
 
 ;; Sample posts
