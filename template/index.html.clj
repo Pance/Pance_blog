0773b66dd20c974c5b44893b2401cda6f064b094
Pance <pbarnett888@gmail.com>
2014-02-21 19:20:11 -0600

    Center text and add links

--- template/index.html.clj
+++ template/index.html.clj
@@ -9,18 +9,25 @@
 
 ; Template is compiled with hiccup
 (header
-  (:title site)
-    [:p.text-center "Hello, I'm Peter Barnett"]
-    [:p.text-center "I am a developer[github] who believes code and design can be beautiful."]
-    [:p.text-center "I work in an accelerated development program as a software developer at HEB Grocery Company[link]."]
+  [:div.text-center (:title site)]
+    [:p.text-center "I am Peter Barnett"]
     [:p.text-center
-      (link "github.com/Pance" "https://github.com/Pance")
+      "I am a "
+      (link "developer" "https://github.com/Pance/")
+      " who believes that code and design can be beautiful."]
+    [:p.text-center "I work in an accelerated development program as a software developer at "
+      (link "HEB Grocery Company" "http://www.heb.com/index.jsp")]
+    [:p.text-center
+      (link "Github.com/Pance" "https://github.com/Pance")
       "  "
       "PBarnett888@gmail.com"
       "  "
-      (link "soundcloud.com/DJPance" "https://soundcloud.com/djpance")]
+      (link "SoundCloud.com/DJPance" "https://soundcloud.com/djpance")]
     [:p.text-center "I am a Texan, hacker, gamer and a bedroom DJ. I am interested in the web, personal data tracking, automation, people, cars, fitness and electronic dance music."]
-    [:p.text-center "I'm currently building a todo-list web app using Clojure called todo-repl[link]."]
+    [:p.text-center "I'm currently building a todo-list web app using "
+     (link "Clojure" "http://clojure.org/")
+     " called "
+     (link "todo-repl" "https://github.com/Pance/todo-repl-webapp")]
 
 	[:br]
   )
